704b4d3d6fb36b138605bc61478b5b6f7d0ee0eb
Gg33 <george.chan84@gmail.com>
2022-09-23 19:54:51 +1000

    click to draw function added

--- Etch-2/main.js
+++ Etch-2/main.js
@@ -1,10 +1,22 @@
 let color = "black";
-
+let click = false;
 
 // Ensures DOM content loaded first. i.e. same as defer in script;
 document.addEventListener("DOMContentLoaded", function() {
     createBoard(16);
     
+    document.querySelector("body").addEventListener("click", function(e){
+        if(e.target.tagName != "BUTTON"){
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if(click){
+                draw.innerHTML = "Now You Can Draw";
+            } else {
+                draw.innerHTML = "You're Not Allowed To Draw";
+            }
+        }
+    })
+
     let btn_popup = document.querySelector("#popup");
     btn_popup.addEventListener("click", function(){
         let size = getSize();
@@ -45,10 +57,12 @@ function getSize() {
 }
 
 function colorDiv() {
-    if(color == "random") {
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    } else {
-        this.style.backgroundColor = 'black';
+    if(click) {
+        if(color == "random") {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor = 'black';
+        }
     }
 }
 
