44746a4bca16f6fc5e4e8dc43a55302f0b6b76ab
Gg33 <george.chan84@gmail.com>
2022-09-25 09:59:39 +1000

    index, main, styles

---
+++ Etch-3/main.js
@@ -0,0 +1,67 @@
+let select = document.querySelector(".size");
+let black = document.querySelector(".black");
+let random = document.querySelector(".random");
+let erase = document.querySelector(".erase");
+let reset = document.querySelector(".reset");
+
+let click = true;
+let color = 'black';
+
+// Get the size board
+select.addEventListener("click", () => {
+    let size = getSize();
+    createBoard(size);
+});
+
+function getSize() {
+    let input = prompt('Enter size grid');
+    let message = document.querySelector(".message");
+
+    if (input === "") {
+        message.innerHTML = "Enter a number";
+    } else if(input < 0 || input > 100 ) {
+        message.innerHTML = "Enter number between 0 and 100";
+    } else {
+        message.innerHTML = "Now you play";
+        return input;
+    }
+}
+
+// Create board 
+function createBoard(size) {
+    let board = document.querySelector(".board");
+    
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    
+    let numDivs = size * size;
+
+    for (let i = 0; i < numDivs; i++) {
+        let div = document.createElement("div");
+        div.style.backgroundColor = 'white';
+        board.insertAdjacentElement("beforeend", div);
+        div.addEventListener("mouseover", colorDiv);
+    }
+}
+
+// Select colors
+function colorDiv() {
+    if(color === 'random'){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else if (color === 'white') {
+        this.style.backgroundColor = 'white';
+    } else {
+        this.style.backgroundColor = 'black';
+    }
+}
+
+
+function setColor(colorChoice) {
+    color = colorChoice;
+}
+
+// create 'click' function 
+function
+
+
+// reset function
